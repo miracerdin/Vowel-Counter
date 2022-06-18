85876b8a88b9832a0b0bef12623fb20d515b7ec1
mirac <miracerdin@gmail.com>
2022-06-18 19:20:11 +0300

    Vowel counter

---
+++ app.js
@@ -0,0 +1,28 @@
+const input = document.querySelector(".input");
+const btn = document.querySelector(".btn");
+const resultDiv = document.querySelector(".result");
+
+const vowels = ["a", "e", "ı", "i", "o", "ö", "u", "ü"];
+const bos = [];
+input.addEventListener("keydown", function (e) {
+  if (e.key == "Enter") {
+    mirac();
+  }
+});
+
+btn.addEventListener("click", mirac);
+
+function mirac() {
+  let sonuc = input.value
+    .toLowerCase()
+    .split("")
+    .filter((x) => vowels.includes(x));
+  if (sonuc.length > 0) {
+    resultDiv.style.display = "block";
+    resultDiv.innerHTML = `${input.value} has ${sonuc.length} vowel `;
+    input.value = "";
+  }
+
+  return sonuc.length;
+}
+console.log(mirac(input.value));
